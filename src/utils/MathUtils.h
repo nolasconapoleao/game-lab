b6f4a3096e7f674783314789e89fa94d698f1073
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-05-15 12:27:59 +0100

    Add support for documentation

--- src/utils/MathUtils.h
+++ src/utils/MathUtils.h
@@ -8,8 +8,31 @@
 
 namespace MathUtils {
 
+/**
+ * @brief Floors the result of a subtraction.
+ * @param minuend first subtraction operand
+ * @param subtrahend second subtraction operand
+ * @param lowerBound min allowed for result
+ * @return clamped result for subtraction
+ */
 uint clamp_sub(uint minuend, uint subtrahend, uint lowerBound);
+
+/**
+ * @brief Ceils the result of an addition.
+ * @param augend first addition operand
+ * @param addend first addition operand
+ * @param upperBound max allowed for result
+ * @return clamped result of an addition
+ */
 uint clamp_add(uint augend, uint addend, uint upperBound);
+
+/**
+ * @brief Clamps the result of a sum.
+ * @param num number to clamp
+ * @param lowerBound min allowed for num
+ * @param upperBound max allowed for num
+ * @return clamped number
+ */
 uint clamp(uint num, uint lowerBound, uint upperBound);
 
 }; // namespace MathUtils
