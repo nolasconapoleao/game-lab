8375e28e65210657e0d895f2509fc80f9ef27e9e
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-06-05 20:44:06 +0100

    Refactor graph methods
    To improve redability the declaration and definition was separated.
    The declaration cannot go into a separate source file without template specialization
    as it fails to compile in the tests

--- src/utils/graph/IGraph.h
+++ src/utils/graph/IGraph.h
@@ -14,48 +14,59 @@ public:
   using EdgeId = std::pair<NodeId, NodeId>;
   using Edge = std::tuple<NodeId, NodeId, EdgeInfo>;
 
-  [[maybe_unused]] bool addNode(const NodeId nodeId, const NodeInfo &node) {
+  // Node operations
+  [[maybe_unused]] [[maybe_unused]] bool addNode(const NodeId nodeId, const NodeInfo &node);
+  void removeNode(const NodeId nodeId);
+  NodeInfo getNode(const NodeId nodeId);
 
-    const auto findNode = [nodeId](const auto node) { return node.first == nodeId; };
-    auto nodeSearched = std::find_if(nodes.begin(), nodes.end(), findNode);
-
-    if (nodeSearched != nodes.end()) {
-      std::cerr << "Node id already in used.\n";
-      return false;
-    }
-
-    nodes.emplace_back(nodeId, node);
-    return true;
-  }
-
-  void removeNode(const NodeId nodeId) {
-    const auto findNode = [nodeId](const auto node) { return node.first == nodeId; };
-    const auto findEdgeConnectedToNode
-        = [nodeId](const auto edge) { return std::get<0>(edge) == nodeId || std::get<1>(edge) == nodeId; };
-
-    edges.erase(std::remove_if(edges.begin(), edges.end(), findEdgeConnectedToNode), edges.end());
-    nodes.erase(std::remove_if(nodes.begin(), nodes.end(), findNode), nodes.end());
-  }
-
-  NodeInfo getNode(const NodeId nodeId) {
-    const auto findNode = [nodeId](const auto node) { return node.first == nodeId; };
-
-    auto node = std::find_if(nodes.begin(), nodes.end(), findNode);
-
-    if (node == nodes.end()) {
-      std::cerr << "Invalid graph node.\n";
-      throw std::out_of_range("Invalid node");
-    }
-    return node->second;
-  }
-
-  virtual EdgeInfo getEdge(const EdgeId &edgeId) = 0;
-  virtual std::vector<NodeId> neighbours(const NodeId nodeId) = 0;
-
+  // Edge operations
   virtual bool addEdge(const EdgeId edgeId, const EdgeInfo &edgeInfo) = 0;
   virtual void removeEdge(const EdgeId &edgeId) = 0;
+  virtual EdgeInfo getEdge(const EdgeId &edgeId) = 0;
+  virtual std::vector<NodeId> neighbours(const NodeId nodeId) = 0;
 
 protected:
   std::vector<Node> nodes;
   std::vector<Edge> edges;
 };
+
+// Method definition
+template <class NodeId, class NodeInfo, class EdgeInfo>
+[[maybe_unused]] bool IGraph<NodeId, NodeInfo, EdgeInfo>::addNode(const NodeId nodeId, const NodeInfo &node) {
+
+  const auto findNode = [nodeId](const auto node) { return node.first == nodeId; };
+  auto nodeSearched = std::find_if(nodes.begin(), nodes.end(), findNode);
+
+  if (nodeSearched != nodes.end()) {
+    std::cerr << "Node id already in used.\n";
+    return false;
+  }
+
+  nodes.emplace_back(nodeId, node);
+  return true;
+}
+
+template <class NodeId, class NodeInfo, class EdgeInfo>
+void IGraph<NodeId, NodeInfo, EdgeInfo>::removeNode(const NodeId nodeId) {
+
+  const auto findNode = [nodeId](const auto node) { return node.first == nodeId; };
+  const auto findEdgeConnectedToNode
+      = [nodeId](const auto edge) { return std::get<0>(edge) == nodeId || std::get<1>(edge) == nodeId; };
+
+  edges.erase(std::remove_if(edges.begin(), edges.end(), findEdgeConnectedToNode), edges.end());
+  nodes.erase(std::remove_if(nodes.begin(), nodes.end(), findNode), nodes.end());
+}
+
+template <class NodeId, class NodeInfo, class EdgeInfo>
+NodeInfo IGraph<NodeId, NodeInfo, EdgeInfo>::getNode(const NodeId nodeId) {
+
+  const auto findNode = [nodeId](const auto node) { return node.first == nodeId; };
+
+  auto node = std::find_if(nodes.begin(), nodes.end(), findNode);
+
+  if (node == nodes.end()) {
+    std::cerr << "Invalid graph node.\n";
+    throw std::out_of_range("Invalid node");
+  }
+  return node->second;
+}
--- src/utils/graph/UndirectedGraph.h
+++ src/utils/graph/UndirectedGraph.h
@@ -8,74 +8,91 @@
 
 template <class NodeId, class NodeInfo, class EdgeInfo>
 class UndirectedGraph : public IGraph<NodeId, NodeInfo, EdgeInfo> {
+
   using Node = typename IGraph<NodeId, NodeInfo, EdgeInfo>::Node;
   using EdgeId = typename IGraph<NodeId, NodeInfo, EdgeInfo>::EdgeId;
   using Edge = typename IGraph<NodeId, NodeInfo, EdgeInfo>::Edge;
 
 public:
-  EdgeInfo getEdge(const EdgeId &edgeId) {
-    const auto findEdge = [edgeId](const auto edge) {
-      return std::get<0>(edge) == edgeId.first && std::get<1>(edge) == edgeId.second
-             || std::get<0>(edge) == edgeId.second && std::get<1>(edge) == edgeId.first;
-    };
-
-    auto edgeFound = std::find_if(this->edges.begin(), this->edges.end(), findEdge);
-
-    if (edgeFound == this->edges.end()) {
-      std::cerr << "Invalid graph edge.\n";
-      throw std::out_of_range("Invalid edge");
-    }
-
-    return std::get<2>(*edgeFound);
-  }
-
-  std::vector<NodeId> neighbours(NodeId nodeId) override {
-
-    std::vector<NodeId> neighbours;
-    for (const auto &edge : this->edges) {
-      if (std::get<0>(edge) == nodeId) {
-        neighbours.emplace_back(std::get<1>(edge));
-      } else if (std::get<1>(edge) == nodeId) {
-        neighbours.emplace_back(std::get<0>(edge));
-      }
-    }
-
-    return neighbours;
-  }
-
-  bool addEdge(const EdgeId edgeId, const EdgeInfo &edgeInfo) override {
-
-    if (edgeId.first == edgeId.second) {
-      std::cerr << "Cannot link node to itself in undirected graph.\n";
-      return false;
-    }
-
-    if (edgeExists(edgeId)) {
-      std::cerr << "Graph already has this connection.\n";
-      return false;
-    }
-
-    this->edges.emplace_back(edgeId.first, edgeId.second, edgeInfo);
-    return true;
-  }
-
-  void removeEdge(const EdgeId &edgeId) override {
-
-    const auto findEdge = [edgeId](const auto edge) {
-      return std::get<0>(edge) == edgeId.first && std::get<1>(edge) == edgeId.second
-             || std::get<0>(edge) == edgeId.second && std::get<1>(edge) == edgeId.first;
-    };
-    this->edges.erase(std::remove_if(this->edges.begin(), this->edges.end(), findEdge), this->edges.end());
-  }
+  // Edge operations
+  bool addEdge(const EdgeId edgeId, const EdgeInfo &edgeInfo) override;
+  void removeEdge(const EdgeId &edgeId) override;
+  EdgeInfo getEdge(const EdgeId &edgeId);
+  std::vector<NodeId> neighbours(NodeId nodeId) override;
 
 private:
-  bool edgeExists(const EdgeId edgeId) {
-    const auto findEdge = [edgeId](const auto edge) {
-      return std::get<0>(edge) == edgeId.first && std::get<1>(edge) == edgeId.second
-             || std::get<0>(edge) == edgeId.second && std::get<1>(edge) == edgeId.first;
-    };
-
-    auto edge = std::find_if(this->edges.begin(), this->edges.end(), findEdge);
-    return edge != this->edges.end();
-  }
+  bool edgeExists(const EdgeId edgeId);
 };
+
+// Method definition
+template <class NodeId, class NodeInfo, class EdgeInfo>
+bool UndirectedGraph<NodeId, NodeInfo, EdgeInfo>::addEdge(const EdgeId edgeId, const EdgeInfo &edgeInfo) {
+
+  if (edgeId.first == edgeId.second) {
+    std::cerr << "Cannot link node to itself in undirected graph.\n";
+    return false;
+  }
+
+  if (edgeExists(edgeId)) {
+    std::cerr << "Graph already has this connection.\n";
+    return false;
+  }
+
+  this->edges.emplace_back(edgeId.first, edgeId.second, edgeInfo);
+  return true;
+}
+
+template <class NodeId, class NodeInfo, class EdgeInfo>
+void UndirectedGraph<NodeId, NodeInfo, EdgeInfo>::removeEdge(const EdgeId &edgeId) {
+
+  const auto findEdge = [edgeId](const auto edge) {
+    return std::get<0>(edge) == edgeId.first && std::get<1>(edge) == edgeId.second
+           || std::get<0>(edge) == edgeId.second && std::get<1>(edge) == edgeId.first;
+  };
+  this->edges.erase(std::remove_if(this->edges.begin(), this->edges.end(), findEdge), this->edges.end());
+}
+
+template <class NodeId, class NodeInfo, class EdgeInfo>
+EdgeInfo UndirectedGraph<NodeId, NodeInfo, EdgeInfo>::getEdge(const EdgeId &edgeId) {
+
+  const auto findEdge = [edgeId](const auto edge) {
+    return std::get<0>(edge) == edgeId.first && std::get<1>(edge) == edgeId.second
+           || std::get<0>(edge) == edgeId.second && std::get<1>(edge) == edgeId.first;
+  };
+
+  auto edgeFound = std::find_if(this->edges.begin(), this->edges.end(), findEdge);
+
+  if (edgeFound == this->edges.end()) {
+    std::cerr << "Invalid graph edge.\n";
+    throw std::out_of_range("Invalid edge");
+  }
+
+  return std::get<2>(*edgeFound);
+}
+
+template <class NodeId, class NodeInfo, class EdgeInfo>
+std::vector<NodeId> UndirectedGraph<NodeId, NodeInfo, EdgeInfo>::neighbours(NodeId nodeId) {
+
+  std::vector<NodeId> neighbours;
+  for (const auto &edge : this->edges) {
+    if (std::get<0>(edge) == nodeId) {
+      neighbours.emplace_back(std::get<1>(edge));
+    } else if (std::get<1>(edge) == nodeId) {
+      neighbours.emplace_back(std::get<0>(edge));
+    }
+  }
+
+  return neighbours;
+}
+
+template <class NodeId, class NodeInfo, class EdgeInfo>
+bool UndirectedGraph<NodeId, NodeInfo, EdgeInfo>::edgeExists(const EdgeId edgeId) {
+
+  const auto findEdge = [edgeId](const auto edge) {
+    return std::get<0>(edge) == edgeId.first && std::get<1>(edge) == edgeId.second
+           || std::get<0>(edge) == edgeId.second && std::get<1>(edge) == edgeId.first;
+  };
+
+  auto edge = std::find_if(this->edges.begin(), this->edges.end(), findEdge);
+  return edge != this->edges.end();
+}
