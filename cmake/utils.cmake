8c85e1a879fe315129e57c379a16e5bf0d1ef26e
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-06-11 15:11:15 +0100

    Add test example target for testing macro states

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,7 +1,10 @@
 ## macro that handles creation of unit test executable
 macro (create_test target)
     ## To allow absolute header paths in includes
-    include_directories(${PROJECT_SOURCE_DIR}/src)
+    include_directories(
+        ${PROJECT_SOURCE_DIR}/src
+        ${CMAKE_BINARY_DIR}/src
+    )
 
     ## To create a target that can run via ctest
     add_test (NAME ${target} COMMAND $<TARGET_FILE:${target}>)
