8cfff6dc4ae724f743c00865321fa05b70cce241
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-06-17 22:14:50 +0100

    Add shell for world class

--- src/model/map/World.h
+++ src/model/map/World.h
@@ -4,5 +4,27 @@
 
 #pragma once
 
-//TODO: nolasconapoleao
-class World {};
+#include "Location.h"
+#include "factory/EntityFactory.h"
+#include "model/entity/Character.h"
+#include "model/entity/Item.h"
+#include "utils/graph/UndirectedGraph.h"
+
+class World {
+public:
+  static CharacterId activeCharacter;
+  static LocationId activeLocation;
+  static void changeFocusedCharacter();
+  void addCharacter(entity::Character character);
+  static entity::Character character(const CharacterId id);
+
+private:
+  static std::vector<entity::Item> items;
+  static std::vector<entity::Character> characters;
+  static UndirectedGraph<LocationId, LocationId, Distance> worldMap;
+
+  friend class EntityFactory;
+  friend class CombatHandler;
+  friend class ItemHandler;
+  friend class TravelHandler;
+};
