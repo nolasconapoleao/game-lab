77f2d8bde12400fa85aa9bd89b88c6cfeef205bc
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-06-17 22:01:20 +0100

    Add common types for game

---
+++ src/common/GameTypes.h
@@ -0,0 +1,19 @@
+//
+// Created by nolasco on 07/06/20.
+//
+
+#pragma once
+
+#include <string>
+
+// Entity
+using Number = uint16_t;
+using Quantity = uint8_t;
+using Size = uint8_t;
+using Distance = uint8_t;
+
+// Database
+using CharacterId = id_t;
+using ItemId = id_t;
+using ResourceId = id_t;
+using LocationId = id_t;
