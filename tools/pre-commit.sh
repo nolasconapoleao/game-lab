9b7e7ae9f7157b090a46488d63e7a8a56b515a41
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-11-21 15:43:35 +0000

    Fix pre commit coding warnings

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-format_file() {
+function format_file() {
   file="${1}"
-  if [[ -f $file ]] && [[ "$file" =~ \.(c|cpp|h|cc)$ ]]; then
-    clang-format -i ${1}
-    git add ${1}
- elif [[ -f $file ]] && [[ "$file" =~ \CMakeLists.(txt)$ ]]; then
-    cmake-format -i ${1}
-    git add ${1}
+  if [[ -f $file ]] && [[ $file =~ \.(c|cpp|h|cc)$ ]]; then
+    clang-format -i "$file"
+    git add "$file"
+  elif [[ -f $file ]] && [[ $file =~ \CMakeLists.(txt)$ ]]; then
+    cmake-format -i "$file"
+    git add "$file"
   fi
 }
 
-case "${1}" in
+case $file in
   --about )
     echo "Run clang-format on source and header files"
     ;;
   * )
-    for file in `git diff-index --cached --name-only HEAD` ; do
-      format_file "${file}"
+    for file in $( exec git diff-index --cached --name-only HEAD ); do
+      format_file "$file"
     done
     ;;
 esac
