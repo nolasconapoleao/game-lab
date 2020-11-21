76bc84f0c2c3869ceb7e921b8750aef115cd6075
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-11-21 13:32:36 +0000

    Format cmake files

--- tools/cmake/tests.cmake
+++ tools/cmake/tests.cmake
@@ -1,6 +1,6 @@
-## macro that handles creation of unit test executable
-macro (create_test target)
-    ## To allow absolute header paths in includes
+# Macro that handles creation of unit test executable
+macro(create_test target)
+    # To allow absolute header paths in includes
     include_directories(
         ${CMAKE_BINARY_DIR}/extras
         ${CMAKE_SOURCE_DIR}/external
@@ -9,6 +9,9 @@ macro (create_test target)
         ${CMAKE_SOURCE_DIR}/src/include
     )
 
-    ## To create a target that can run via ctest
-    add_test (NAME ${target} COMMAND $<TARGET_FILE:${target}>)
-endmacro (create_test)
+    # To create a target that can run via ctest
+    add_test(
+        NAME ${target}
+        COMMAND $<TARGET_FILE:${target}>
+    )
+endmacro(create_test)
