e9b66df4dfd250df3dc93ba1be8aa95243e162bd
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-11-22 01:11:49 +0000

    Correct cmake-format file
    Correct world wide CMakeLists files

--- tools/cmake/tests.cmake
+++ tools/cmake/tests.cmake
@@ -1,17 +1,14 @@
 # Macro that handles creation of unit test executable
 macro(create_test target)
-    # To allow absolute header paths in includes
-    include_directories(
-        ${CMAKE_BINARY_DIR}/extras
-        ${CMAKE_SOURCE_DIR}/external
-        ${CMAKE_SOURCE_DIR}/extras
-        ${CMAKE_SOURCE_DIR}/src/game
-        ${CMAKE_SOURCE_DIR}/src/include
-    )
+  # To allow absolute header paths in includes
+  include_directories(
+    ${CMAKE_BINARY_DIR}/extras
+    ${CMAKE_SOURCE_DIR}/external
+    ${CMAKE_SOURCE_DIR}/extras
+    ${CMAKE_SOURCE_DIR}/src/game
+    ${CMAKE_SOURCE_DIR}/src/include
+  )
 
-    # To create a target that can run via ctest
-    add_test(
-        NAME ${target}
-        COMMAND $<TARGET_FILE:${target}>
-    )
+  # To create a target that can run via ctest
+  add_test(NAME ${target} COMMAND $<TARGET_FILE:${target}>)
 endmacro(create_test)
