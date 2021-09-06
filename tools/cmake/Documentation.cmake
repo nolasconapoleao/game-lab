c3cee6a5f46c54b35d485eb4fc2f10434cbd1486
Nolasco Napoleao <nolascoamadonapoleao@gmail.com>
2021-09-06 19:10:01 +0100

    Remove cmake lib

--- tools/cmake/Documentation.cmake
+++
@@ -1,8 +0,0 @@
-# Documentation target
-set(DOCUMENTATION_TARGET Documentation)
-add_custom_target(
-  ${DOCUMENTATION_TARGET}
-  COMMAND doxygen
-  COMMENT "Generate documentation"
-  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/docs
-)
